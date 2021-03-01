3f8c79a713011c3321e9e92098d48e515bb00703
Kate Zhao <katez@andrew.cmu.edu>
2021-03-01 11:56:07 -0500

    added completeListItem functionality

--- main.js
+++ main.js
@@ -14,3 +14,11 @@ function deleteListItem(item) {
     // remove li element (item) from ol element (item.parentNode)
     item.parentNode.removeChild(item);
 }
+
+function completeListItem(item) {
+    if (item.checked) {
+        item.parentNode.style.textDecoration = "line-through";
+    } else {
+        item.parentNode.style.textDecoration = "none";
+    }
+}
